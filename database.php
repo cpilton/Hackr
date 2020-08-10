9f0bbee375853989539d10dbc0bc4531725bd3fd
Callum <pilton.callum@gmail.com>
2020-08-10 20:49:49 +0100

    Add database connection

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = $_ENV["SQL_HOST"]; // MySQL host name eg. shareddb-p.hosting.stackcp.net
+$user = $_ENV["DQL_USER"]; // MySQL user. eg. root ( if your on localserver)
+$password = $_ENV["SQL_PASSWORD"]; // MySQL user password  (if password is not set for your root user then keep it
+// empty )
+$database = $_ENV["SQL_DATABASE"]; // MySQL Database name
+
+$DBH = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+ 
+// Connect to MySQL Database
+$con = new mysqli($host, $user, $password, $database);
+// Callum Pilton
